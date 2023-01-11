8de6461431561c8541dbaa5d16dfbb72f144ff41
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-01-11 21:17:46 +0530

    nf: square_dd.c

---
+++ square/square_dd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    for (i = 5; i >= 1; i--) {
+        for (j = 5; j >= 1; j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+// To print i instead of *, replace the following code by code on line 7
+// printf("%d", i);
