560b3f2646e2d9f1118331ffb7992af17f24526d
Pranav <pranavprajapati586@gmail.com>
2023-01-15 10:51:18 +0530

    nf:mirror_half_pyramid_di.c

---
+++ 4. Mirror Half Pyramid/mirror_half_pyramid_di.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, k;
+
+    for (i = 5; i >= 1; i--) {
+        for (k = 1; k <= (i - 1); k++) {
+            printf(" ");
+        }
+
+        for (j = i; j <= 5; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
