6cc3113071da50f05b54ef8ac12d51e7feb362ae
Pranav <pranavprajapati586@gmail.com>
2023-01-15 22:50:29 +0530

    nf:inverted_mirror_half_pyramid_di.c

---
+++ 5. Inverted Mirror Half Pyramid/inverted_mirror_half_pyramid_di.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, k;
+
+    for (i = 5; i >= 1; i--) {
+
+        for (k = 5; k >= i; k--) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
