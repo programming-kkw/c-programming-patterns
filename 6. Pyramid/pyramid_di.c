6355db751aa5aaba6e22b8a8c5c7f78877129345
Pranav <pranavprajapati586@gmail.com>
2023-01-17 00:53:24 +0530

    nf:pyramid_di.c

---
+++ 6. Pyramid/pyramid_di.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    int i, j, k;
+
+    for (i = 5; i >= 1; i--) {
+        for (k = 1; k < i; k++) {
+            printf(" ");
+        }
+
+        for (j = 2*i; j <=10; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+}
