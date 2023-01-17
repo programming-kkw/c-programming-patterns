68710daaf1f7a957709bf8080e6772afb44f50cb
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-01-17 23:31:36 +0530

    nf: inverted_pyramid_id.c

---
+++ 7. Inverted Pyramid/inverted_pyramid_id.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    int i, j, k;
+
+    for (i = 1; i <= 5; i++) {
+        for (k = 1; k < i; k++) {
+            printf(" ");
+        }
+
+        for (j = ((2 * (5 - i) ) + 1); j >= 1; j--) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+}
