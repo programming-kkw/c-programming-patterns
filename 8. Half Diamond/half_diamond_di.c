50d273c9e0efc49318fc0e44fd4f92a42a7bbe73
Pranav <pranavprajapati586@gmail.com>
2023-01-19 20:59:38 +0530

    nf:half_diamond_di.c

---
+++ 8. Half Diamond/half_diamond_di.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int i,j;
+    for ( i = 5; i >= 1; i--){
+        for (j = 5; j > i; j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+     for(i = 1; i<= 5; i++){
+        for(j = i; j <= 5; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+    
