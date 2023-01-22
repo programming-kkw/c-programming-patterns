861fee4e2dd621e10dc4288430bc16beaab16126
Pranav <pranavprajapati586@gmail.com>
2023-01-23 00:11:37 +0530

    fix:optimized and understandable code for diamond_id.c

--- 10. Diamond/diamond_id.c
+++ 10. Diamond/diamond_id.c
@@ -8,19 +8,19 @@ int main () {
             printf(" ");
         }
 
-        for (j = ((2 * i) - 1); j >= 1; j--) {
+        for (j = 2*i; j > 1; j--) {
             printf("*");
         }
 
         printf("\n");
     }
 
-    for (i = 1; i <= 5; i++) {
-        for (k = 0; k < i; k++) {
+    for (i = 2; i <= 5; i++) {
+        for (k = 1; k < i; k++) {
             printf(" ");
         }
 
-        for (j = 2 * (5 - i); j > 1; j--) {
+        for (j = 10; j >= 2*i; j--) {
             printf("*");
         }
 
